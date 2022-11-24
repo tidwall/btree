90442f7bc7ef0198158cedfed8b3ec4768355145
Gergely Novak <novak.r.gergely@gmail.com>
2022-11-24 10:06:25 +0100

    Put Deprecated comments to new paragraph

--- btreeg.go
+++ btreeg.go
@@ -1306,18 +1306,21 @@ func (tr *BTreeG[T]) Clear() {
 }
 
 // Generic BTree
+//
 // Deprecated: use BTreeG
 type Generic[T any] struct {
 	*BTreeG[T]
 }
 
 // NewGeneric returns a generic BTree
+//
 // Deprecated: use NewBTreeG
 func NewGeneric[T any](less func(a, b T) bool) *Generic[T] {
 	return &Generic[T]{NewBTreeGOptions(less, Options{})}
 }
 
 // NewGenericOptions returns a generic BTree
+//
 // Deprecated: use NewBTreeGOptions
 func NewGenericOptions[T any](less func(a, b T) bool, opts Options) *Generic[T] {
 	return &Generic[T]{NewBTreeGOptions(less, opts)}
