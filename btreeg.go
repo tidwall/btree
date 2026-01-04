5d2331604e509e5e4e596522a336d2b8e0c59951
yihuang <yi.huang@mantrachain.io>
2026-01-04 11:30:47 +0800

    Update btreeg.go

--- btreeg.go
+++ btreeg.go
@@ -1553,7 +1553,7 @@ func (tr *BTreeG[T]) Copy() *BTreeG[T] {
 }
 
 func (tr *BTreeG[T]) IsoCopy() *BTreeG[T] {
-  var mu *sync.RWMutex
+	var mu *sync.RWMutex
 	if tr.lock(!tr.readOnly) {
 		mu = new(sync.RWMutex)
 		defer tr.unlock(!tr.readOnly)
